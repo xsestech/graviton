73c1e6d0b6d5ff760a27055e0130d7afdbed2afc
xsestech <xsestech@gmail.com>
2023-12-22 19:08:18 +0300

    Added config file to maneuver

--- src/KSP/maneuver_and_collect.py
+++ src/KSP/maneuver_and_collect.py
@@ -1,21 +1,18 @@
 import os
 import time
+
+import joblib
 import krpc
 import matplotlib.pyplot as plt
-import joblib
+
+from src.KSP.config import get_ksp_settings
 
 # Конфиг
-DATA_PATH = '../../data/'
-DATA_DELAY = 0.1
-WAIT_TIME = 2
-WARP_FACTOR = 4
-TARGET_PERIAPSIS = 250e3
-N_REQUESTS = 1000
-IS_CHEAT_ON = False
+settings = get_ksp_settings()
 
 # Создаем папку для сохранения данных
-if not os.path.exists(DATA_PATH):
-    os.mkdir(DATA_PATH)
+if not os.path.exists(settings.DATA_PATH):
+    os.mkdir(settings.DATA_PATH)
 
 # Подключаемся к серверу krpc
 conn = krpc.connect()
@@ -23,23 +20,23 @@ spc = conn.space_center
 vessel = conn.space_center.active_vessel
 
 # Доводим орбиту до нужной
-if IS_CHEAT_ON:
+if settings.IS_CHEAT_ON:
     # Отделяем лишнюю ступень и включаем САС
     vessel.control.activate_next_stage()
     vessel.control.sas = True
-    time.sleep(WAIT_TIME)
+    time.sleep(settings.WAIT_TIME)
 
     # Меняем вектор направления ракеты на "по движению"
-    for _ in range(N_REQUESTS):
+    for _ in range(settings.N_REQUESTS):
         vessel.control.sas_mode = vessel.control.sas_mode.prograde
-    time.sleep(WAIT_TIME)
+    time.sleep(settings.WAIT_TIME)
     vessel.control.throttle = 1.0
 
     # Держим двигатели включенными, пока не достигнем нужной высоты в перигее
     periapsis_altitude = conn.get_call(getattr, vessel.orbit, 'periapsis_altitude')
     expr = conn.krpc.Expression.greater_than(
         conn.krpc.Expression.call(periapsis_altitude),
-        conn.krpc.Expression.constant_double(TARGET_PERIAPSIS))
+        conn.krpc.Expression.constant_double(settings.TARGET_PERIAPSIS))
     event = conn.krpc.add_event(expr)
     with event.condition:
         event.wait()
@@ -60,21 +57,21 @@ ut = conn.add_stream(getattr, conn.space_center, 'ut')
 work = True
 try:
     # Данный блок немного костыльный, но по-другому гарантировано установить ускорение времени не получилось
-    time.sleep(WAIT_TIME)
-    for _ in range(N_REQUESTS):
-        spc.rails_warp_factor = WARP_FACTOR
+    time.sleep(settings.WAIT_TIME)
+    for _ in range(settings.N_REQUESTS):
+        spc.rails_warp_factor = settings.WARP_FACTOR
     # Запускаем цикл снятия данных
     while work:
         cords.append(position())
         velocities.append(velocity())
         times.append(ut())
-        time.sleep(DATA_DELAY)
+        time.sleep(settings.DATA_DELAY)
 except KeyboardInterrupt:  # Остановка по ctl-c
     spc.rails_warp_factor = 0  # Выключим ускорение времени
     # Сохраним массивы с помощью joblib
-    joblib.dump(cords, os.path.join(DATA_PATH, "cords_2.joblib"))
-    joblib.dump(velocities, os.path.join(DATA_PATH, "velocities_2.joblib"))
-    joblib.dump(times, os.path.join(DATA_PATH, "time_2.joblib"))
+    joblib.dump(cords, os.path.join(settings.DATA_PATH, "cords_2.joblib"))
+    joblib.dump(velocities, os.path.join(settings.DATA_PATH, "velocities_2.joblib"))
+    joblib.dump(times, os.path.join(settings.DATA_PATH, "time_2.joblib"))
 
     # Выведем график
     plt.plot(velocities)
